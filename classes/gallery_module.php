49a3ad977af1172558596673bbdfffa89e8f6e52
Eric Muyser <eric@muyser.com>
2011-06-23 14:54:49 -0700

    add events.

--- classes/gallery_module.php
+++ classes/gallery_module.php
@@ -1,6 +1,10 @@
 <?
 
+	define('PATH_MOD_GALLERY', PATH_APP . '/modules/gallery');
+	
 	class Gallery_Module extends Core_ModuleBase {
+		const PATH = PATH_MOD_GALLERY;
+		
 		protected function createModuleInfo() {
 			return new Core_ModuleInfo(
 				"Gallery",
@@ -15,25 +19,27 @@
 		
 		public function buildPermissionsUi($host_obj) {
 			$host_obj->add_field($this, 'galleries', 'Manage galleries', 'left')->renderAs(frm_checkbox)->comment('View and manage the update.', 'above');
-
 		}
 		
 		public function listTabs($tabCollection) {
 			$user = Phpr::$security->getUser();
+			
 			$tabs = array(
 				'set_items' => array('set_items', 'Set Items', 'set_items'),
 				'settings' => array('settings', 'Settings', 'settings')
 			);
 
 			$first_tab = null;
-			foreach($tabs as $tab_id=>$tab_info) {
+			
+			foreach($tabs as $tab_id => $tab_info) {
 				if(($tabs[$tab_id][3] = $user->get_permission('gallery', $tab_info[2])) && !$first_tab)
 					$first_tab = $tab_info[0];
 			}
 
 			if($first_tab) {
 				$tab = $tabCollection->tab('gallery', 'Gallery', $first_tab, 30);
-				foreach($tabs as $tab_id=>$tab_info) {
+				
+				foreach($tabs as $tab_id => $tab_info) {
 					if($tab_info[3])
 						$tab->addSecondLevel($tab_id, $tab_info[1], $tab_info[0]);
 				}
--- models/gallery_set_item.php
+++ models/gallery_set_item.php
@@ -7,6 +7,8 @@
 			'images' => array('class_name' => 'Db_File', 'foreign_key' => 'master_object_id', 'conditions' => "master_object_class='Gallery_Set_Item' and field='images'", 'order' => 'sort_order, id', 'delete' => true)
 		);
 		
+		protected $api_added_columns = array();
+		
 		public static function create() {
 			return new self();
 		}
@@ -18,6 +20,10 @@
 			$this->define_column('sort_order', 'Sort Order')->validation()->fn('trim')->unique("This sort order is already in use");
 			$this->define_column('enabled', 'Enabled');
 			$this->define_multi_relation_column('images', 'images', 'Images', '@name')->invisible();
+			
+			$this->defined_column_list = array();
+			Backend::$events->fireEvent('gallery:onExtendSetItemModel', $this, $context);
+			$this->api_added_columns = array_keys($this->defined_column_list);
 		}
 
 		public function define_form_fields($context = null)
@@ -32,6 +38,15 @@
 			$editor_config->apply_to_form_field($field);
 			
 			$this->add_form_field('images')->renderAs(frm_file_attachments)->renderFilesAs(frm_file_attachments)->renderFilesAs('image_list')->addDocumentLabel('Add image(s)')->tab('Images')->noAttachmentsLabel('There are no images uploaded')->noLabel()->imageThumbSize(555)->fileDownloadBaseUrl(url('ls_backend/files/get/'));
+			
+			Backend::$events->fireEvent('gallery:onExtendSetItemForm', $this, $context);
+			
+			foreach($this->api_added_columns as $column_name) {
+				$form_field = $this->find_form_field($column_name);
+				
+				if($form_field)
+					$form_field->optionsMethod('get_added_field_options');
+			}
 		}
 		
 		public static function set_orders($item_ids, $item_orders) {
