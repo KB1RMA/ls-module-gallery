fe506a63d357ab1785330d05dfd0bbe1dbd0cb36
Chris Snyder <chris@keyedupmedia.com>
2013-05-23 16:17:57 -0400

    Add page_url() for ease of finding the page URL. Similar to Shop_Category

--- models/gallery_set_item.php
+++ models/gallery_set_item.php
@@ -2,13 +2,13 @@
 
 	class Gallery_Set_Item extends Db_ActiveRecord {
 		public $table_name = 'gallery_set_items';
-		
+
 		public $has_many = array(
 			'images' => array('class_name' => 'Db_File', 'foreign_key' => 'master_object_id', 'conditions' => "master_object_class='Gallery_Set_Item' and field='images'", 'order' => 'sort_order, id', 'delete' => true)
 		);
-		
+
 		protected $api_added_columns = array();
-		
+
 		public static function create() {
 			return new self();
 		}
@@ -22,7 +22,7 @@
 			$this->define_column('enabled', 'Enabled');
 			$this->define_multi_relation_column('images', 'images', 'Images', '@name')->invisible();
 			$this->define_column('slug', 'Slug')->invisible()->validation()->fn('trim');
-			
+
 			$this->defined_column_list = array();
 			Backend::$events->fireEvent('gallery:onExtendSetItemModel', $this, $context);
 			$this->api_added_columns = array_keys($this->defined_column_list);
@@ -38,27 +38,27 @@
 			$field = $this->add_form_field('description')->tab('Set Item');
 			$field->renderAs(frm_html)->size('small');
 			$editor_config->apply_to_form_field($field);
-			
+
 			$editor_config = System_HtmlEditorConfig::get('gallery', 'gallery_set_item_description');
 			$field = $this->add_form_field('long_description')->tab('Set Item');
 			$field->renderAs(frm_html)->size('small');
 			$editor_config->apply_to_form_field($field);
-			
+
 			$this->add_form_field('images')->renderAs(frm_file_attachments)->renderFilesAs('image_list')->addDocumentLabel('Add image(s)')->tab('Images')->noAttachmentsLabel('There are no images uploaded')->noLabel()->imageThumbSize(555)->fileDownloadBaseUrl(url('ls_backend/files/get/'));
-			
+
 			Backend::$events->fireEvent('gallery:onExtendSetItemForm', $this, $context);
-			
+
 			foreach($this->api_added_columns as $column_name) {
 				$form_field = $this->find_form_field($column_name);
-				
+
 				if($form_field)
 					$form_field->optionsMethod('get_added_field_options');
 			}
 		}
-		
+
 		public function get_added_field_options($db_name, $current_key_value = -1) {
 			$result = Backend::$events->fireEvent('gallery:onGetSetItemFieldOptions', $db_name, $current_key_value);
-			
+
 			foreach($result as $options) {
 				if(is_array($options) || (strlen($options && $current_key_value != -1)))
 					return $options;
@@ -66,11 +66,11 @@
 
 			return false;
 		}
-		
+
 		public static function set_orders($item_ids, $item_orders) {
 			if(is_string($item_ids))
 				$item_ids = explode(',', $item_ids);
-				
+
 			if(is_string($item_orders))
 				$item_orders = explode(',', $item_orders);
 
@@ -84,7 +84,7 @@
 
 			Cms_Module::update_cms_content_version();
 		}
-		
+
 		public function after_create() {
 			Db_DbHelper::query('update gallery_set_items set sort_order=:sort_order where id=:id', array(
 				'sort_order' => $this->id,
@@ -92,5 +92,22 @@
 			));
 
 			$this->sort_order = $this->id;
+		}
+
+		public function page_url($default)
+		{
+			$page_url = Cms_PageReference::get_page_url($this, 'page_id', $this->page_url);
+
+			$url_name = $this->slug;
+			if (substr($url_name, 0, 1) == '/')
+				$url_name = substr($url_name, 1);
+
+			if (!strlen($page_url))
+				return root_url($default.'/'.$url_name);
+
+			if (!strlen($url_name))
+				return root_url($page_url);
+
+			return root_url($page_url.'/'.$url_name);
 		}
 	}
